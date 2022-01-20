51d9621bcdd6b9baa271042fe8961fb4368f6506
ZC <carter.zo3y@gmail.com>
2022-01-20 15:56:29 -0800

    rehost in clean repo

---
+++ README.md
@@ -0,0 +1,5 @@
+# [Timestamp Microservice](https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/timestamp-microservice)
+
+API serving JSON `unix` timestamp and `utc` date upon request. Empty request returns current time.
+
+Date parsing provided by `new Date()` per project specs, response may vary between browsers based on implementation of nonstandard parsing.
---
+++ server.js
@@ -0,0 +1,51 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/api/:date?', function (req, res) {
+  let targetDate = dateValParser(req.params.date);
+  let body;
+  let code;
+  if (isNaN(targetDate.getTime())) {
+    code = 400;
+    body = {error: 'Invalid Date'};
+  } else {
+    code = 200;
+    body = {
+      unix: targetDate.getTime(),
+      utc: targetDate.toUTCString(),
+    }
+  }
+  res.status(code).json(body);
+});
+
+function dateValParser(param) {
+  let dateVal = param;
+  if (dateVal === undefined) {
+    return new Date(Date.now());
+  } else if (!isNaN(Number(dateVal))) {
+    return new Date(Number(dateVal));
+  }
+  return new Date(dateVal);
+}
+
+// listen for requests :)
+app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
